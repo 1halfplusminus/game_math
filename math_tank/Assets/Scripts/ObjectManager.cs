46a8230d81df9dbf73615999b3d140050d7d70a8
dartuchiwa <dartuchiwa@gmail.com>
2020-01-18 20:07:31 +1100

    Feat - tank finish

---
+++ math_tank/Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour
+{
+    GameObject objectInstance;
+    public GameObject objPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var position = new Vector3(Random.Range(-100, 100), Random.Range(-100, 100), objPrefab.transform.position.z);
+        objectInstance = Instantiate(objPrefab, position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public GameObject GetObject()
+    {
+        return objectInstance;
+    }
+}
---
+++ math_tank/Assets/UI/Ui.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ui : MonoBehaviour
+{
+    public Transform tankPosition;
+    public ObjectManager fuelManager;
+    public Text tankPositionText;
+    public Text fuelPositionText;
+    public Text distanceText;
+    public Text energyText;
+
+    public Drive drive;
+
+    public void AddEnergy(string amount)
+    {
+        int n;
+        if (int.TryParse(amount, out n))
+        {
+            drive.AddEnergy(n);
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tankPositionText.text = tankPosition.position.ToString();
+        fuelPositionText.text = fuelManager.GetObject().transform.position.ToString();
+        energyText.text = drive.GetEnergy().ToString();
+    }
+
+    private static string PositionToString(Vector3 position)
+    {
+        return "(" + (int)position.x + "," + (int)position.y + ")";
+    }
+}
