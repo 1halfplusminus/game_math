3700b8b76514a195bedb25573e049ca56726c5f5
Onehalfplusminus <gael.sautron@si2p.nc>
2020-01-22 12:58:22 +1100

    Feat - ray

---
+++ ray_1/Assets/Line.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line
+{
+    Coords a;
+    Coords b;
+    Coords v;
+
+    public Line(Coords a, Coords b)
+    {
+        this.a = a;
+        this.b = b;
+        this.v = b - a;
+    }
+
+    public Coords GetPointAt(float t)
+    {
+        return a + (v * t);
+    }
+}
